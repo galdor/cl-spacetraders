c246c2eab209602ded745470b34f10c76d8f340a
Nicolas Martyanoff <nicolas@n16f.net>
2023-05-11 21:45:06 +0200

    start exporting symbols

--- src/package.lisp
+++ src/package.lisp
@@ -6,5 +6,37 @@
    #:*api-uri*
    #:*authentication-token*
 
+   #:point
+   #:point-x
+   #:point-y
+   #:serialize-point
+
+   #:waypoint
+   #:waypoint-symbol
+   #:waypoint-type
+   #:waypoint-point
+
+   #:system
+   #:system-symbol
+   #:system-sector-symbol
+   #:system-type
+   #:system-point
+   #:system-waypoints
+
+   #:load-systems
+   #:find-system
+
+   #:faction-trait
+   #:faction-trait-symbol
+   #:faction-trait-name
+   #:faction-trait-description
+
+   #:faction
+   #:faction-symbol
+   #:faction-name
+   #:faction-description
+   #:faction-headquarters
+   #:faction-traits
+
    #:register
    #:find-faction))
